a2bec9ae142e0288f9c5b942d36a9346d6bd391b
unknown <marcelobettini@yahoo.com.ar>
2022-05-14 19:58:52 -0300

    table details

--- src/components/PokemonTable.jsx
+++ src/components/PokemonTable.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import PokemonRow from "./PokemonRow";
 import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material"
 
+
 const PokemonTable = ({ data, filter, setSelectedPokemon }) => {
     return (
+
         <TableContainer component={Paper} sx={{ display: 'flex', mt: '2em' }}>
             <Table >
                 <TableHead>
                     <TableRow>
-                        <TableCell align="center">Name</TableCell>
-                        <TableCell align="center">Type</TableCell>
+                        <TableCell >Name</TableCell>
+                        <TableCell >Type</TableCell>
                         <TableCell align="center">Action</TableCell>
                     </TableRow>
                 </TableHead>
